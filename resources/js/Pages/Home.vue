a24a36e26216cc293df461b89065579c65bd0db9
Raphael Reis <reis.rrsc@outlook.com>
2026-05-16 14:20:50 -0300

    chore: initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Bem-vindo ao ServiceHub</h1>
+        <p>Vue.js e Inertia configurados com sucesso!</p>
+    </div>
+</template>
+
+<script setup>
+// Lógica do componente aqui
+</script>
